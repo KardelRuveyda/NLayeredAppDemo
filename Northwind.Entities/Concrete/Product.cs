56e3db39ef41f12593722b365404916bb129df31
KardelRuveyda <ruveydakardelcetin@gmail.com>
2021-01-25 22:03:33 +0300

    Fluent Validation

--- Northwind.Entities/Concrete/Product.cs
+++ Northwind.Entities/Concrete/Product.cs
@@ -11,7 +11,7 @@ namespace Northwind.Entities.Concrete
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public string CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public decimal UnitPrice { get; set; }
         public string QuantityPerUnit { get; set; }
         public Int16 UnitsInStock { get; set; }
