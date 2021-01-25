2c1c769d3dcd0beacc8163bd91e1fbe0368a4dc2
KardelRuveyda <ruveydakardelcetin@gmail.com>
2021-01-25 21:33:12 +0300

    Add project files.

---
+++ Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,50 @@
+﻿using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected Int32 Sum(int one, int two)
+        {
+            int result;
+            int tmp;
+
+            tmp = one + two;
+            result = tmp;
+            return result;
+        }
+
+    }
+}
